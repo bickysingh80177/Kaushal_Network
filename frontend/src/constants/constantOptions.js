541080e16f2a6f81897dcc25a5c9dd8d1742c54e
Bicky Singh <bicky.singh6510@gmail.com>
2024-02-09 10:55:57 +0530

    options added

--- frontend/src/constants/constantOptions.js
+++ frontend/src/constants/constantOptions.js
@@ -12,5 +12,69 @@ export const entityOptions = [
   { id: 3, name: "Partnership", value: "partnership" },
   { id: 4, name: "Proprietorship", value: "proprietorship" },
   { id: 5, name: "Joint Venture", value: "joint_venture" },
-  { id: 6, name: "Other Forms", value: "others" },
+  { id: 6, name: "Foreign Country", value: "foreign_country" },
+  { id: 7, name: "Other (pls specify)", value: "others" },
+];
+
+export const businessInvOptions = [
+  { id: 1, name: "Goods", value: "goods" },
+  { id: 2, name: "Services", value: "services" },
+  { id: 3, name: "Trade", value: "trade" },
+  { id: 4, name: "Securities", value: "securities" },
+  { id: 5, name: "Investments", value: "investements" },
+  { id: 6, name: "E-Commerce", value: "e-commerce" },
+  { id: 7, name: "Digital Products", value: "digital_products" },
+  { id: 8, name: "Financing", value: "financing" },
+  { id: 9, name: "Other (pls specify)", value: "others" },
+];
+
+export const sectorOptions = [
+  { id: 1, name: "Agriculture", value: "agriculture" },
+  { id: 2, name: "Service", value: "service" },
+  { id: 3, name: "Manufacturing", value: "manufacturing" },
+  { id: 4, name: "Research", value: "research" },
+  { id: 5, name: "Construction", value: "construction" },
+  { id: 6, name: "Tourism", value: "tourism" },
+  { id: 7, name: "Power", value: "power" },
+  { id: 8, name: "Mining", value: "mining" },
+  { id: 9, name: "Others (pls specify)", value: "others" },
+];
+
+export const industryOptions = [
+  { id: 1, name: "Automotive Industry", value: "automotive" },
+  { id: 2, name: "Textile Industry", value: "textile" },
+  { id: 3, name: "Pharmaceutical Industry", value: "pharmaceutical" },
+  { id: 4, name: "Chemical Industry", value: "chemical" },
+  { id: 5, name: "Steel Industry", value: "steel" },
+  { id: 6, name: "Electronics Industry", value: "electronics" },
+  { id: 7, name: "Real Estate", value: "real_estate" },
+  { id: 8, name: "Infrastructure Development", value: "infrastructure" },
+  { id: 9, name: "Information Technology (IT) Services", value: "IT" },
+  { id: 10, name: "Banking and Finance", value: "banking/finance" },
+  { id: 11, name: "Telecommunications", value: "telecommunications" },
+  { id: 12, name: "Retail", value: "retail" },
+  { id: 13, name: "Healthcare", value: "healthcare" },
+  { id: 14, name: "Hospitality", value: "hospitality" },
+  { id: 15, name: "Travel Agencies", value: "travel_agencies" },
+  { id: 16, name: "Research and Development", value: "R&D" },
+  { id: 17, name: "Education", value: "education" },
+  { id: 18, name: "Information Technology Consulting", value: "IT_consulting" },
+  { id: 19, name: "Public Services", value: "public_service" },
+  { id: 20, name: "Crop cultivation", value: "crop" },
+  { id: 21, name: "Animal husbandry", value: "animal_husbandry" },
+  { id: 22, name: "Professional Service", value: "professional_service" },
+  { id: 23, name: "Legal", value: "legal" },
+  { id: 24, name: "Digital", value: "digital" },
+  { id: 25, name: "Others (pls specify)", value: "others" },
+];
+
+export const empCntOptions = [
+  { id: 1, name: "0-10", value: "0-10" },
+  { id: 2, name: "10-20", value: "10-20" },
+  { id: 2, name: "20-50", value: "20-50" },
+  { id: 2, name: "50-100", value: "50-100" },
+  { id: 2, name: "100-500", value: "100-500" },
+  { id: 2, name: "500-1000", value: "500-1000" },
+  { id: 2, name: "1000-5000", value: "1000-5000" },
+  { id: 2, name: "more than 5000", value: "more than 5000" },
 ];
