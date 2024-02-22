64c1b111218cac9d236f1551ec43b10d2d925d42
Bicky Singh <bicky.singh6510@gmail.com>
2024-02-22 22:56:58 +0530

    home page mobile development

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -8,10 +8,10 @@ const Home = () => {
     <div>
       <Navbar />
       <div className="mb-8">
-        <h1 className="lg:text-4xl lg:mt-5 flex justify-center font-extralight">
+        <h1 className="xs:mt-4 xs:text-sm lg:text-4xl lg:mt-5 flex justify-center font-extralight">
           Join Kaushal Network
         </h1>
-        <h3 className="lg:text-xl lg:mt-5 flex justify-center font-extralight">
+        <h3 className="xs:text-sm lg:text-xl lg:mt-5 flex justify-center font-extralight">
           Find MSMEs, Service Providers, Corporates
         </h3>
         {/* search group */}
@@ -21,7 +21,7 @@ const Home = () => {
             <input type="text" placeholder="Industry, Sector Type" />
             <input type="text" placeholder="Location" />
           </div>
-          <button className="sm:mt-10 sm:px-2 sm:w-24 sm:h-8 sm:gap-2 sm:py-1 bg-[#253974] text-white rounded-md lg:px-4 lg:py-2 text-sm lg:p-auto hover:bg-[#3E63DD] hover:text-gray-400 lg:ms-8 flex items-center justify-center gap-4 h-10 w-32">
+          <button className="xs:mt-8 sm:mt-10 sm:px-2 sm:w-24 sm:h-8 sm:gap-2 sm:py-1 bg-[#253974] text-white rounded-md lg:px-4 lg:py-2 text-sm lg:p-auto hover:bg-[#3E63DD] hover:text-gray-400 lg:ms-8 flex items-center justify-center gap-4 h-10 w-32">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -44,17 +44,17 @@ const Home = () => {
             <h2 className="top-section">Services</h2>
             <div className="top-section-details">Top services</div>
           </div>
-          <div className="sm:w-3/4 sm:mt-12 lg:w-2/6">
+          <div className="xs:mt-8 sm:w-3/4 sm:mt-12 lg:w-2/6">
             <h2 className="top-section">MSMEs</h2>
             <div className="top-section-details">Top services</div>
           </div>
-          <div className="sm:w-3/4 sm:mt-12 lg:w-2/6">
+          <div className="xs:mt-8 sm:w-3/4 sm:mt-12 lg:w-2/6">
             <h2 className="top-section">Corporates</h2>
             <div className="top-section-details">Top services</div>
           </div>
         </div>
         <div className="flex flex-col items-center sm:mt-4 lg:mt-8">
-          <h3 className="sm:text-xl lg:text-2xl font-medium">
+          <h3 className="xs:text-center xs:text-base sm:text-xl lg:text-2xl font-medium">
             Sponsered Businesses, Service Providers, MSMEs
           </h3>
           <div className="w-5/6 bg-red-100 h-80 mt-4 p-1 rounded-md">
